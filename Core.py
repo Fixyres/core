15b1819903f18b204b5688e7eb9ee91fb43bf4a2
Артем <158840682+Fixyres@users.noreply.github.com>
2024-05-27 18:55:13 +0300

    Update Core.py

--- Core.py
+++ Core.py
@@ -16,7 +16,7 @@ API_KEY = '41d63e00422b4ab1a39a77d6582b79c1'
 
 moscow_tz = pytz.timezone('Europe/Moscow')
 
-TOKEN = '6775251060:AAGl7SdVCZcbFCUhWFpaCW2DTMMKiOrVqJk'
+TOKEN = '6775251060:AAFhio2O5X19pfmI858j8mOhmoWKkchwZpM'
 bot = telebot.TeleBot(TOKEN)
 
 BASE_URL = 'https://openexchangerates.org/api/'
